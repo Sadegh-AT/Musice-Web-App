cbf80e22d2bc8397bcd4da8ec3714adb314c2f7e
SadeghAT <sadegh.artivox@gmail.com>
2023-03-08 23:08:58 +0330

    make header

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,23 @@
-import "../css/main.css";
-console.log("Hello");
+function Darkmode(darkmode) {
+  darkmode.addEventListener("click", function () {
+    if (localStorage.getItem("color-theme") != "dark") {
+      localStorage.setItem("color-theme", "dark");
+      darkmode.innerHTML = "Light";
+    } else {
+      localStorage.setItem("color-theme", "light");
+      darkmode.innerHTML = "Dark";
+    }
+    SetDarkorLight();
+  });
+}
+function SetDarkorLight() {
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
