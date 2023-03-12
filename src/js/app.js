014c2af3b92c40cfddeede2850fe0d988d3e5f9b
SadeghAT <sadegh.artivox@gmail.com>
2023-03-13 00:01:00 +0330

    fix darkmode icon

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 const darkmodeBtn = document.querySelector("#darkmodeBtn");
-
+const fonticon = darkmodeBtn.querySelector("span");
 SetDarkorLight();
 Darkmode(darkmodeBtn);
 function Darkmode(darkmode) {
   darkmode.addEventListener("click", function () {
     if (localStorage.getItem("color-theme") != "dark") {
       localStorage.setItem("color-theme", "dark");
-      const fonticon = darkmode.querySelector("span");
+
       fonticon.innerHTML = `<i class="fa-solid fa-sun"></i>`;
     } else {
       localStorage.setItem("color-theme", "light");
-      const fonticon = darkmode.querySelector("span");
+
       fonticon.innerHTML = `<i class="fa-solid fa-moon"></i>`;
     }
     SetDarkorLight();
@@ -23,7 +23,9 @@ function SetDarkorLight() {
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     document.documentElement.classList.add("dark");
+    fonticon.innerHTML = `<i class="fa-solid fa-sun"></i>`;
   } else {
     document.documentElement.classList.remove("dark");
+    fonticon.innerHTML = `<i class="fa-solid fa-moon"></i>`;
   }
 }
