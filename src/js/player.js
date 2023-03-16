eec8b703bec5ca06a1b62ebb64f7b0cb5dfc4bf5
SadeghAT <sadegh.artivox@gmail.com>
2023-03-16 22:48:50 +0330

    make progress bar

--- src/js/player.js
+++ src/js/player.js
@@ -1,21 +1,38 @@
-const audioPlayer = document.getElementById('audio-player');
-const playBtn = document.getElementById("play")
+const audioPlayer = document.getElementById("audio-player");
+const playBtn = document.getElementById("play");
+
+const timeElapsed = document.getElementById("time-elapsed");
+const songDuration = document.getElementById("song-duration");
+
+const playerTrack = document.getElementById("player-track");
 
 playBtn.addEventListener("click", function () {
-    playAndPause()
-})
+  playAndPause();
+});
 
+audioPlayer.addEventListener("timeupdate", function () {
+  const currentTime = audioPlayer.currentTime;
+  const duration = audioPlayer.duration;
+  const progressPercent = (currentTime / duration) * 100;
+
+  playerTrack.value = progressPercent;
+  timeElapsed.innerHTML = formatTime(currentTime);
+  songDuration.innerHTML = formatTime(duration);
+});
 
 function playAndPause() {
-    const btnIcon = playBtn.querySelector("i")
-    if (btnIcon.className == "fa-solid fa-play") {
-        
-        btnIcon.className = "fa-solid fa-pause"
-        
-    } else {
-        btnIcon.className = "fa-solid fa-play"
-        
-        
-    }
-    
+  const btnIcon = playBtn.querySelector("i");
+  if (btnIcon.className == "fa-solid fa-play") {
+    btnIcon.className = "fa-solid fa-pause";
+    audioPlayer.play();
+  } else {
+    btnIcon.className = "fa-solid fa-play";
+    audioPlayer.pause();
+  }
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
