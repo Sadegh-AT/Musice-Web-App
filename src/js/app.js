2042bbf2b38b093b74764e175b985736e6255b04
SadeghAT <sadegh.artivox@gmail.com>
2023-03-17 20:26:46 +0330

    select music and play

--- src/js/app.js
+++ src/js/app.js
@@ -31,18 +31,17 @@ function SetDarkorLight() {
 }
 
 const musicTable = document.querySelector("#music-table");
-let elementHtml = `<tbody
-                        class="mb-4 flex w-full cursor-pointer dark:text-[#0C0C0C] dark:hover:bg-[#d4d4d4] dark:border-[#D2D2D2] justify-between rounded-lg border-2 border-[#404040] p-2 text-white opacity-80 transition-all hover:bg-[#3e3e3e]">
-                        <tr class="flex space-x-7">
-                            <td>1</td>
-                            <td>1</td>
-                            <td>Prologue</td>
-                        </tr>
-                        <tr class="flex space-x-8">
-                            <td>3:45</td>
-                            <td>6.66 MB</td>
-                            <td>17.92 MB</td>
-                        </tr>
-                    </tbody>`;
 
-new MusicTrack(musicTable, elementHtml, 100);
+new MusicTrack(musicTable, [
+  "Prologue",
+  "Firelink Shrine",
+  "Taurus Demon",
+  "Bell Gargoyle",
+]);
+
+function selectMusic(element) {
+  const name = element.querySelector(".musicName");
+  console.log(`./music/${name.innerHTML}`);
+  audioPlayer.src = `./music/${name.innerHTML + ".mp3"}`;
+  playAndPause();
+}
--- src/js/musictarck.js
+++ src/js/musictarck.js
@@ -1,12 +1,24 @@
 class MusicTrack {
-  constructor(container, elementHtml, number) {
-    this.addTrack(container, elementHtml, number);
+  constructor(container, musicName) {
+    this.addTrack(container, musicName);
   }
 
-  addTrack(container, elementHtml, number) {
-    let element = elementHtml;
-    for (let index = 0; index < number; index++) {
-      container.insertAdjacentHTML("beforeend", element);
+  addTrack(container, musicName) {
+    for (let index = 0; index < musicName.length; index++) {
+      let elementHtml = `<tbody onclick="selectMusic(this)"
+                        class="mb-4 flex w-full cursor-pointer dark:text-[#0C0C0C] dark:hover:bg-[#d4d4d4] dark:border-[#D2D2D2] justify-between rounded-lg border-2 border-[#404040] p-2 text-white opacity-80 transition-all hover:bg-[#3e3e3e]">
+                        <tr class="flex space-x-7">
+                            <td>1</td>
+                            <td>${index + 1}</td>
+                            <td class="musicName">${musicName[index]}</td>
+                        </tr>
+                        <tr class="flex space-x-8">
+                            <td>3:45</td>
+                            <td>6.66 MB</td>
+                            <td>17.92 MB</td>
+                        </tr>
+                    </tbody>`;
+      container.insertAdjacentHTML("beforeend", elementHtml);
     }
   }
 }
